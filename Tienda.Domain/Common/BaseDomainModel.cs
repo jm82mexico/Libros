32b0849da4351953fddde880c5d94b7e7b2ad128
Juan Del Angel <jmdelan2012@gmail.com>
2024-03-31 22:46:01 +0000

    Agregado BaseDomainModel y ValueObject en Tienda.Domain.Common

---
+++ Tienda.Domain/Common/BaseDomainModel.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Tienda.Domain.Common
+{
+    public class BaseDomainModel
+    {
+        public int Id { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public string? CreatedBy { get; set; }
+        public DateTime? LastModifiedDate { get; set; }
+        public string? LastModifiedBy { get; set; }
+    }
+}
