662f91515da32309944bfb1cf2a726880688ed54
Juan Del Angel <jmdelan2012@gmail.com>
2024-04-01 00:15:28 +0000

    Actualizar firma de método GetAsync en IAsyncRepository.cs

--- Tienda.Application/Contracts/Persistence/IAsyncRepository.cs
+++ Tienda.Application/Contracts/Persistence/IAsyncRepository.cs
@@ -9,13 +9,13 @@ namespace Tienda.Application.Contracts.Persistence
 
         Task<IReadOnlyList<T>> GetAsync(Expression<Func<T, bool>> predicate);
 
-        Task<IReadOnlyList<T>> GetAsync(Expression<Func<T, bool>> predicate = null,
-                       Func<IQueryable<T>, IOrderedQueryable<T>> orderBy = null,
-                                            string includeString = null,
+        Task<IReadOnlyList<T>> GetAsync(Expression<Func<T, bool>>? predicate = null,
+                       Func<IQueryable<T>, IOrderedQueryable<T>>? orderBy = null,
+                                            string? includeString = null,
                                              bool disableTracking = true);
-        Task<IReadOnlyList<T>> GetAsync(Expression<Func<T, bool>> predicate = null,
-                                    Func<IQueryable<T>, IOrderedQueryable<T>> orderBy = null,
-                                    List<Expression<Func<T, object>>> includes = null,
+        Task<IReadOnlyList<T>> GetAsync(Expression<Func<T, bool>>? predicate = null,
+                                    Func<IQueryable<T>, IOrderedQueryable<T>>? orderBy = null,
+                                    List<Expression<Func<T, object>>>? includes = null,
                                     bool disableTracking = true);
 
         Task<T> GetByIdAsync(int id);
