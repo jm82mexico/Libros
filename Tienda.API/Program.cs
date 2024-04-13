fcf5a83b9c979e34be4d46080b9e3216983bc0b1
Juan Del Angel <jmdelan2012@gmail.com>
2024-04-13 23:58:29 +0000

    Actualizar configuración de la API en program.cs

--- Tienda.API/Program.cs
+++ Tienda.API/Program.cs
@@ -9,116 +9,124 @@ using Microsoft.OpenApi.Models;
 using Tienda.API.Middlewares;
 using Microsoft.AspNetCore.Mvc.Versioning;
 using Microsoft.AspNetCore.Mvc.ApiExplorer;
-
-var builder = WebApplication.CreateBuilder(args);
-
-// Add services to the container.
-
-// Inicialización de Serilog 
-    Log.Logger = new LoggerConfiguration()
-        .ReadFrom.Configuration(builder.Configuration)
-        .CreateLogger();
-
-#region Common API Services
-builder.Services.AddControllers().AddJsonOptions(options =>
+try
 {
-    options.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter());
-});
 
-builder.Services.AddApiVersioning(opt =>
-{
-    opt.DefaultApiVersion = new Microsoft.AspNetCore.Mvc.ApiVersion(1, 0);
-    opt.AssumeDefaultVersionWhenUnspecified = true;
-    opt.ReportApiVersions = true;
-    opt.ApiVersionReader = ApiVersionReader.Combine(
-        new UrlSegmentApiVersionReader(),
-        new HeaderApiVersionReader("x-api-version"),
-        new MediaTypeApiVersionReader("x-api-version"));
-});
-builder.Services.AddVersionedApiExplorer(setup =>
-{
-    setup.GroupNameFormat = "'v'VVV";
-    setup.SubstituteApiVersionInUrl = true;
-});
+    var builder = WebApplication.CreateBuilder(args);
 
+    // Add services to the container.
 
+    // Inicialización de Serilog 
+        Log.Logger = new LoggerConfiguration()
+            .ReadFrom.Configuration(builder.Configuration)
+            .CreateLogger();
 
-builder.Host.UseSerilog();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen(c =>
-{
-    var xmlFile = $"{System.Reflection.Assembly.GetExecutingAssembly().GetName().Name}.xml";
-    var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
-    c.IncludeXmlComments(xmlPath, true);
-
-    c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+    #region Common API Services
+    builder.Services.AddControllers().AddJsonOptions(options =>
     {
-        Description = "Autenticación JWT utilizando el esquema Bearer. Ejemplo: \"Bearer {token}\"",
-        Name = "Authorization",
-        In = ParameterLocation.Header,
-        Type = SecuritySchemeType.Http,
-        Scheme = "Bearer",
-        BearerFormat = "JWT"
+        options.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter());
     });
-    c.OperationFilter<JwtAuthorizationOperationFilter>();
-});
 
-builder.Services.AddHttpContextAccessor();
-builder.Services.ConfigureOptions<ConfigureSwaggerOptions>();
-builder.Services.AddTransient<GlobalExceptionHandlingMiddleware>();
-builder.Services.AddHttpClient();
-#endregion
-
-#region Services Registration
-builder.Services.AddApplicationServices();
-builder.Services.AddInfrastructureServices(builder.Configuration);
-builder.Services.AddIdentityServices(builder.Configuration);
-#endregion
-
-builder.Services.AddCors(options =>
+    builder.Services.AddApiVersioning(opt =>
     {
-        options.AddPolicy("CorsPolicy", policy =>
+        opt.DefaultApiVersion = new Microsoft.AspNetCore.Mvc.ApiVersion(1, 0);
+        opt.AssumeDefaultVersionWhenUnspecified = true;
+        opt.ReportApiVersions = true;
+        opt.ApiVersionReader = ApiVersionReader.Combine(
+            new UrlSegmentApiVersionReader(),
+            new HeaderApiVersionReader("x-api-version"),
+            new MediaTypeApiVersionReader("x-api-version"));
+    });
+    builder.Services.AddVersionedApiExplorer(setup =>
+    {
+        setup.GroupNameFormat = "'v'VVV";
+        setup.SubstituteApiVersionInUrl = true;
+    });
+
+
+
+    builder.Host.UseSerilog();
+    // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+    builder.Services.AddEndpointsApiExplorer();
+    builder.Services.AddSwaggerGen(c =>
+    {
+        var xmlFile = $"{System.Reflection.Assembly.GetExecutingAssembly().GetName().Name}.xml";
+        var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+        c.IncludeXmlComments(xmlPath, true);
+
+        c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
         {
-            policy.AllowAnyHeader()
-            .AllowAnyMethod()
-            .AllowAnyOrigin();
+            Description = "Autenticación JWT utilizando el esquema Bearer. Ejemplo: \"Bearer {token}\"",
+            Name = "Authorization",
+            In = ParameterLocation.Header,
+            Type = SecuritySchemeType.Http,
+            Scheme = "Bearer",
+            BearerFormat = "JWT"
         });
-});
-
-var app = builder.Build();
-var apiVersionDescriptionProvider = app.Services.GetRequiredService<IApiVersionDescriptionProvider>();
-app.UseSerilogRequestLogging();
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI(options =>
-    {
-        foreach (var description in apiVersionDescriptionProvider.ApiVersionDescriptions.Reverse())
-        {
-            options.SwaggerEndpoint($"/swagger/{description.GroupName}/swagger.json",
-                description.GroupName.ToUpperInvariant());
-        }
+        c.OperationFilter<JwtAuthorizationOperationFilter>();
     });
+
+    builder.Services.AddHttpContextAccessor();
+    builder.Services.ConfigureOptions<ConfigureSwaggerOptions>();
+    builder.Services.AddTransient<GlobalExceptionHandlingMiddleware>();
+    builder.Services.AddHttpClient();
+    #endregion
+
+    #region Services Registration
+    builder.Services.AddApplicationServices();
+    builder.Services.AddInfrastructureServices(builder.Configuration);
+    builder.Services.AddIdentityServices(builder.Configuration);
+    #endregion
+
+    builder.Services.AddCors(options =>
+        {
+            options.AddPolicy("CorsPolicy", policy =>
+            {
+                policy.AllowAnyHeader()
+                .AllowAnyMethod()
+                .AllowAnyOrigin();
+            });
+    });
+
+    var app = builder.Build();
+    var apiVersionDescriptionProvider = app.Services.GetRequiredService<IApiVersionDescriptionProvider>();
+
+    app.UseSerilogRequestLogging();
+
+    // Configure the HTTP request pipeline.
+    if (app.Environment.IsDevelopment())
+    {
+        app.UseSwagger();
+        app.UseSwaggerUI(options =>
+        {
+            foreach (var description in apiVersionDescriptionProvider.ApiVersionDescriptions.Reverse())
+            {
+                options.SwaggerEndpoint($"/swagger/{description.GroupName}/swagger.json",
+                    description.GroupName.ToUpperInvariant());
+            }
+        });
+    }
+    if (!app.Environment.IsDevelopment())
+    {
+        app.UseHsts();
+    }
+    else
+    {
+        app.UseDeveloperExceptionPage();
+    }
+    app.UseHttpsRedirection();
+    app.UseAuthorization();
+    app.UseAuthentication();
+    app.UseMiddleware<GlobalExceptionHandlingMiddleware>();
+    app.UseCors("CorsPolicy");
+    app.MapControllers();
+    app.Run();
 }
-if (!app.Environment.IsDevelopment())
+catch (Exception ex)
 {
-    app.UseHsts();
+    Log.Fatal(ex, "API terminated unexpectedly");
 }
-else
+finally
 {
-    app.UseDeveloperExceptionPage();
+    Log.CloseAndFlush();
 }
-
-app.UseHttpsRedirection();
-
-app.UseAuthentication();
-
-app.UseAuthorization();
-
-app.UseCors("CorsPolicy");
-
-app.MapControllers();
-
-app.Run();
